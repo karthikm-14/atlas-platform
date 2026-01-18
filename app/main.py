9c493504a529e6a114a06d0580f4098e6592e5ec
Karthik.Mahadevaiah <karthik.mahadevaiah@MS-IBLR-11131.local>
2026-01-18 22:42:45 +0530

    Day 2 done – request & response models understood.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1 import health
+from app.api.v1 import health, ping
 
 app = FastAPI()
 
 app.include_router(health.router, prefix="/api/v1")
+app.include_router(ping.router, prefix="/api/v1")
 
 @app.get("/")
 def root():
