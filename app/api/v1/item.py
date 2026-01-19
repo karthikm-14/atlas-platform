8fe76ccc65b2a64845cddf94b0330cd4ca768202
Karthik.Mahadevaiah <karthik.mahadevaiah@MS-IBLR-11131.local>
2026-01-19 15:23:38 +0530

    Day 4 done – models vs schemas clear.

--- app/api/v1/item.py
+++ app/api/v1/item.py
@@ -2,19 +2,20 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.models.item import Item
+from app.schemas.item import ItemCreate, ItemResponse
 
 
 router = APIRouter()
 
-@router.get("/items")
+@router.get("/items", response_model=list[ItemResponse])
 def list_items(db: Session = Depends(get_db)):
     return db.query(Item).all()
 
 
-@router.post("/items")
-def create_item(name: str, db: Session = Depends(get_db)):
-    item = Item(name=name)
-    db.add(item)
+@router.post("/items", response_model=ItemResponse)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(name=item.name)
+    db.add(db_item)
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_item)
+    return db_item
